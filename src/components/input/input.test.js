b1db704abd52b5cde6aff110120ee7d61605e6c9
AnasShar <anas10895@gmail.com>
2021-03-28 22:46:39 +0300

    adding check Props test

--- src/components/input/input.test.js
+++ src/components/input/input.test.js
@@ -1,21 +1,30 @@
 import React from 'react'
-import findByTestAttr from '../../test/testUtil'
+import {findByTestAttr, checkProp} from '../../test/testUtil'
 import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 import Enzyme,{shallow} from 'enzyme';
 import Input from './index'
 
 Enzyme.configure({ adapter: new Adapter() });
 
-// const defaultProps = {
-//     input:""
-// }
+/**
+ * 
+ * Factory Function to create a Input  for Input Component 
+ * @function setup 
+ * @param {opject} props - Component props specific  for this component
+ * @returns {shallowWrapper} 
+ */
 
-// const setup = (props={}) => {
+const secretWord="party"
 
-//     const setupProps = {...defaultProps, ...props}
-//     return shallow(<input {...setupProps } />)
-// } 
-
+const setup = (props={}) => {
+    return shallow(<Input secretWord={secretWord}/>)
+}
 test("render without error", () => {
+    const warpper = setup();
+    const component = findByTestAttr(warpper, "component-input")
+    expect(component.length).toBe(1)
+})
 
+test('dose not throw error with unexpected props', () => {
+    checkProp(Input, {secretWord:"test"})
 })
